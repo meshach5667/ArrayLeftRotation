58acc110e06ee447cc23e2475f7b6e3f447d61c6
ydbwala <yati.bwala@binghamuni.edu.ng>
2024-06-11 14:35:04 +0100

    I created the array left rotation file

---
+++ ArrayLeftRotation.java
@@ -0,0 +1,16 @@
+public class ArrayLeftRotation {
+
+    public static int[] rotateLeft(int[] a, int d){
+        int n = a.length;
+        int[] rotatedArray = new int[n];
+
+        for (int i = 0; i < n; i++) {
+
+            int newPosition =(i + n- d)% n;
+            rotatedArray[newPosition] = a[i];
+            
+        }
+        return rotatedArray;
+
+    }
+}
